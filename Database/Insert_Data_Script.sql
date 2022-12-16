593eba104f65296100fa7c83bad0958e1904565d
a-wow <107396951+a-wow@users.noreply.github.com>
2022-12-16 21:45:14 +0300

    Insert_Data_Script.sql

---
+++ Database/Insert_Data_Script.sql
@@ -0,0 +1,3 @@
+CALL admin_account_creation('solbonka182','admin','mr.ect9@gmail.com', 1);
+CALL admin_account_creation('admin','qwerty','solbon18@yandex.ru', 1);
+CALL account_creation('test','test','mr.ect@mail.ru');

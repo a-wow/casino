cda7516ee638be094dc0acec0d102a3bf2da0ebf
a-wow <107396951+a-wow@users.noreply.github.com>
2022-12-16 23:38:35 +0300

    restoreAccount.php

---
+++ src/php/restoreAccount.php
@@ -0,0 +1,22 @@
+<?php
+/**
+  * Создано A-WoW.
+  * Пользователь: Солбон
+  * Дата: 09-12-222
+  * Время: 19:15
+ */
+
+include("../../includes/dbConnection.php");
+
+$id = $_POST['id'];
+
+$sql = "UPDATE accounts SET is_deleted=0 WHERE id=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute() === FALSE) {
+    echo "Error: " . $conn->error;
+}
+
+$stmt->close();
+$conn->close();
